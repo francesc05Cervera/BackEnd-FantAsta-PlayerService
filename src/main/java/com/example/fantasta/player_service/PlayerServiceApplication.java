e30213fad0c61cd7f22b482401fb897bb261034e
francesco <francescocervera2005@gmail.com>
2026-07-07 16:47:21 +0200

    Base progetto

---
+++ src/main/java/com/example/fantasta/player_service/PlayerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasta.player_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fantasta/player_service/PlayerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasta.player_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
